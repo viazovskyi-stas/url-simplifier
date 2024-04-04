e551f6ed7cfedb91348bd3cd2cd098fc4e5da2d0
Viazovskyi Stas <viazovskyi.stas@gmail.com>
2024-04-04 15:39:18 +0300

    css fixes

--- src/app/_components/LinkList.tsx
+++ src/app/_components/LinkList.tsx
@@ -20,9 +20,9 @@ const LinkList = () => {
     <table className='text-surface  min-w-full text-left text-sm font-light dark:text-white'>
       <thead className='flex w-full border-b border-neutral-200 font-medium dark:border-white/10'>
         <tr className='mb-4 flex w-full'>
-          <th className='w-1/6 p-4'>#</th>
-          <th className='w-1/4 p-4'>Original URL</th>
-          <th className='w-1/4 p-4'>Short URL</th>
+          <th className='w-1/8 p-4'>#</th>
+          <th className='w-1/3 p-4'>Original URL</th>
+          <th className='w-1/3 p-4'>Short URL</th>
           <th className='w-1/4 p-4'>Created At</th>
         </tr>
       </thead>
@@ -30,8 +30,8 @@ const LinkList = () => {
         {(data?.data.list || []).map((item, index) => {
           return (
             <tr className='mb-4 flex w-full' key={item._id}>
-              <td className='w-1/6 p-4'>{index}</td>
-              <td className='w-1/4 p-4'>
+              <td className='w-1/8 p-4'>{index}</td>
+              <td className='w-1/3 p-4'>
                 <Link
                   href={item.original_url}
                   rel='noopener noreferrer'
@@ -40,7 +40,7 @@ const LinkList = () => {
                   {item.original_url}
                 </Link>
               </td>
-              <td className='w-1/4 p-4'>
+              <td className='w-1/3 p-4'>
                 <Link
                   href={item.short_url}
                   rel='noopener noreferrer'
