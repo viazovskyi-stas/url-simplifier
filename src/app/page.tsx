42eb18093e9e17b108bd716ab33ca63d7b05ed5a
Viazovskyi Stas <viazovskyi.stas@gmail.com>
2024-04-04 16:34:33 +0300

    style fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import CreateShortUrlForm from '@/src/app/_components/CreateShortUrlForm';
 
 export default function Home() {
   return (
-    <main className='flex min-w-[60rem] flex-col items-center justify-center gap-5 rounded-md bg-transparent bg-white bg-opacity-40 p-5'>
+    <main className='flex min-w-[60rem] h-[600px] flex-col items-center justify-start gap-5 rounded-md bg-transparent bg-white bg-opacity-40 p-5'>
       <h1>URL Simplifier</h1>
       <CreateShortUrlForm />
       <LinkList />
